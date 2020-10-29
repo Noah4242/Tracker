e8287ce9f3be1291a83e55e5dbd952c01979a282
Noah <mad@dbcs-m.k12k.com>
2020-10-29 11:56:03 -0400

    Add app folder

---
+++ app/screens/main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Text, View ,Image,StyleSheet,} from "react-native";
+
+return(
+<View>
+<View>
+    <Text>calories burned</Text>
+    <Text> daily budget</Text>
+    <Text>Calories consumed</Text>
+</View>
+</View>
+)
