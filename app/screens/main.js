9d28015f1d0084c893917ce62ee6cf9d4decae61
Noah <mad@dbcs-m.k12k.com>
2020-10-30 12:07:17 -0400

    it works now

--- app/screens/main.js
+++ app/screens/main.js
@@ -1,12 +1,33 @@
 import React from "react";
-import {Text, View ,Image,StyleSheet,} from "react-native";
-
-return(
-<View>
-<View>
-    <Text>calories burned</Text>
-    <Text> daily budget</Text>
-    <Text>Calories consumed</Text>
-</View>
-</View>
-)
+import {
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  Button,
+} from "react-native";
+export default function App() {
+  return (
+    <SafeAreaView>
+      <View
+        style={{
+          backgroundColor: "red",
+          flexDirection: "row",
+          height: "30%",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text>calories burned</Text>
+        <Text> daily budget</Text>
+        <Text>Calories consumed</Text>
+      </View>
+      <View></View>
+      <View>
+        <Button title="BUTTON"></Button>
+        <Button title="BUTTON"></Button>
+        <Button title="BUTTON"></Button>
+      </View>
+    </SafeAreaView>
+  );
+}
