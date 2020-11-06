636ec47b0f51becc58063621ae2d9864017db93f
Noah <mad@dbcs-m.k12k.com>
2020-11-06 12:10:09 -0500

    kf

--- app/screens/main.js
+++ app/screens/main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Svg, { Circle, Rect } from "react-native-svg";
+import Svg, { Circle, Path, Rect } from "react-native-svg";
 
 import {
   Text,
@@ -11,36 +11,38 @@ import {
 } from "react-native";
 export default function App() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ backgroundColor: "teal" }}>
       <View
         style={{
           backgroundColor: "red",
           flexDirection: "row",
           height: "15%",
           justifyContent: "space-between",
+          alignItems: "center",
         }}
       >
         <Text>calories burned</Text>
         <Text> daily budget</Text>
         <Text>Calories consumed</Text>
       </View>
-      <View>
-        <Svg height="50%" width="50%" viewBox="0 0 100 100">
-          <Circle
-            cx="150"
-            cy="30"
-            r="45"
-            stroke="blue"
-            strokeWidth="2.5"
-            fill="green"
-          />
-          <Circle
-            cx="50"
-            cy="50"
-            r="42"
-            stroke="black"
-            strokeWidth="2.5"
-            fill="coral"
+      <View
+        style={{
+          backgroundColor: "teal",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Svg
+          width="100%"
+          height="200"
+          viewBox="0 0 300 300"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <Circle cx="100" cy="100" r="100" fill="#3B38C6" />
+          <Path
+            d="M100 0C82.07 -2.13813e-07 64.4699 4.82073 49.0425 13.9574C33.615 23.0941 20.9279 36.2106 12.3096 51.9334C3.69129 67.6563 -0.541192 85.4071 0.055365 103.327C0.651922 121.247 6.05557 138.677 15.7005 153.792C25.3454 168.907 38.8767 181.151 54.8776 189.241C70.8785 197.332 88.7604 200.971 106.651 199.779C124.541 198.586 141.781 192.606 156.567 182.463C171.353 172.321 183.139 158.389 190.693 142.128L100 100V0Z"
+            fill="#F0A202"
           />
         </Svg>
       </View>
