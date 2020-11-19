546430d5b3b276e915db1f1e9280f0c42ee810b5
Noah Williams <noahswitch42@gmail.com>
2020-11-19 12:04:08 -0500

    yarn yarn

--- app/screens/add.js
+++ app/screens/add.js
@@ -1,4 +1,16 @@
 const { View } = require("react-native");
+import React from "react";
+import Svg, { Circle, Path, Rect } from "react-native-svg";
+
+import {
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 
 <View>
     <View>
